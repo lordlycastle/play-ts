9394bbd08ad57a2cb29e85457f5919fb34ceed93
lordlycastle <>
2021-06-04 00:59:48 +0200

    I'm wondering over here

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,10 +1,47 @@
+import { flow } from 'fp-ts/function';
+import { curry } from 'ramda';
 import { expect } from './shared/expectations.spec';
 import { ExcludeMatchingProperties } from './main';
+import * as sinon from 'sinon';
 
 describe('main.ts', function() {
+  describe('', () => {
+    it('should fails', function() {
+      expect(true).to.be.false;
+    });
+  })
   it('should exclude the mentioned properties', function() {
     const wholeObject = {a: 1, b: 2, c: 3};
     const excludeMatchingProperties: ExcludeMatchingProperties<typeof wholeObject, 'b' | 'c'> = wholeObject;
     expect({a: 1}).to.be.deep.equal(excludeMatchingProperties)
+  });
+});
+
+describe('stubbing curried functions', function() {
+  const _ = {
+     threeArgFunc: (a: string, b: string, c: number) => `${a}-${b}-${c}`,
+    twoArgFunc: (b: string, c: number) => `${b}***${c}`,
+  }
+   function testFunction(addSuffix?: boolean): string {
+     const curriedFunc = addSuffix
+       ? curry(_.threeArgFunc)(`suffix: ${addSuffix} //`)
+       : curry(_.twoArgFunc)  ;
+     return flow(curriedFunc)('hello', 42);
+   }
+
+  it('should add suffix', function() {
+    const suffixResult: string = 'suffix: true // ...';
+    const threeArgFuncStub = sinon.stub(_, 'threeArgFunc').resolves(suffixResult);
+    const result = testFunction(true);
+    expect(result).to.eql(suffixResult);
+    expect(threeArgFuncStub.calledOnce).to.be.true;
+  });
+
+  it('should not add suffix', function() {
+    const expResult: string = '...';
+    const threeArgFuncStub = sinon.stub(_, 'twoArgFunc').resolves(expResult);
+    const result = testFunction(true);
+    expect(result).to.eql(result);
+    expect(threeArgFuncStub.calledOnce).to.be.true;
   });
 });
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,53 @@
+import { flow, pipe } from 'fp-ts/function';
+import { curry } from 'ramda';
 import { llog } from './shared/llog';
+import { apS, flatten } from 'fp-ts/Array';
 
 export type ExcludeMatchingProperties<T, V> = Pick<
   T,
   { readonly [K in keyof T]-?: T[K] extends V ? never : K }[keyof T]
 >;
 
+/**
+ * Exclude from T those types that are assignable to U
+ */
+type Exclude<T, U> = T extends U ? never : T;
+
+// Object Types
+type Oa = {
+  a: string;
+  b: number;
+};
+type Ob = {
+  b: number;
+  c: boolean;
+};
 type A = 11 | 22 | 3 | 4;
 type B = 1 | 2 | 4;
 const x: ExcludeMatchingProperties<A, B> = 4;
 llog(x);
+
+// apS from fp-ts
+// export declare const myApS: <N, A, B>(
+//   name: Exclude<N, keyof A>,
+//   fb: B[]
+// ) => (
+//   fa: A[]
+// ) => { readonly [K in N | keyof A]: K extends keyof A ? A[K] : B }[];
+
+// llog(apS())
+
+const someFlag: boolean | undefined = true;
+async function flowCurried(): Promise<string> {
+  // Q: HOW does this work? The curry from both requests 3 or 2 args?
+  return flow(
+    curry(
+      someFlag
+        ? (a: string, b: number, c: boolean) => `${a}-${b}-${String(c)}`
+        : (b: number, c: boolean) => `$NO-a//-${b}-${String(c)}`
+    )
+  )(1, false);
+
+}
+
+llog(await flowCurried());
